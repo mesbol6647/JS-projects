db3e67b0fd103bbd5639861ac79699e825490dc9
mesbol <mesbol6647@gmail.com>
2023-11-07 19:53:38 +0300

    last change

--- TeamWork-todo/project_2/app.js
+++ TeamWork-todo/project_2/app.js
@@ -1,6 +1,14 @@
-// var kare = document.querySelectorAll(".bosKare");
-// kare.forEach(function(bosKare) {
-//     bosKare.addEventListener("click", function() {
-//         bosKare.classList.toggle("kare");
-//     });
-// });
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+const gorevTexts = document.querySelectorAll(".gorev-text");
+
+checkboxes.forEach(function(checkbox, index) {
+    checkbox.addEventListener("change", function() {
+        if (checkbox.checked) {
+            gorevTexts[index].style.textDecoration = "line-through";
+            gorevTexts[index].style.color = "black";
+        } else {
+            gorevTexts[index].style.textDecoration = "none";
+            gorevTexts[index].style.color = "black";
+        }
+    });
+});
--- TeamWork-todo/project_4/app.js
+++ TeamWork-todo/project_4/app.js
@@ -1,43 +1,61 @@
-const addButton = document.getElementById("add-button");
-const todoContainer = document.getElementById("todo-container");
-const todoInput = document.getElementById("todo-input");
 
-addButton.addEventListener("click", (e) => {
-  let todo = document.createElement("div");
-  todo.classList.add("todo");
+   let yeniGorev = document.getElementById("yeniGorev")
+   let gorevListesi = document.getElementById("gorevListesi");
+   
+   let li = document.createElement("li");
+   let tick = document.createElement("i");
+   let deleteIcon = document.createElement("i");
+   let gorevText = document.createElement("span");
+   let completed = false; 
+  
 
-  let li = document.createElement("li");
-  li.innerText = `${todoInput.value}`;
-  todo.appendChild(li);
-
-  let check = document.createElement("i");
-  check.innerHTML ='<i class="fa-light fa-square-check"></i>';
-  check.classList.add("todo-check");
-  todo.appendChild(check);
-
-  let deleteButton = document.createElement("button");
-  deleteButton.innerHTML = '<i class="fa-solid fa-circle-minus" ></i>';
-  deleteButton.classList.add("todo-delete");
-  todo.appendChild(deleteButton);
-
-  if (todoInput.value === "") {
-    alert("Please Enter Some Text");
+function gorevEkle() {
+  let yeniGorev = document.getElementById("yeniGorev").value;
+  if (yeniGorev.trim() === "") {
+      alert("Boş bir görev ekleyemezsiniz.");
   } else {
-    todoContainer.appendChild(todo);
-  }
+    let li = document.createElement("li");
+    let tick = document.createElement("i");
+    let deleteIcon = document.createElement("i");
+    let gorevText = document.createElement("span");
+    let completed = false; 
 
-  todoInput.value = "";
-});
+      li.classList.add("animate");
+      tick.classList.add( "fas", "fa-light", "fa-square-check","tick");
+      deleteIcon.classList.add("fas", "fa-times", "delete");
+      gorevText.textContent = yeniGorev;
 
-todoContainer.addEventListener("click", (e) => {
-  let target = e.target;
-  if (target.classList.contains("todo-delete")) {
-    let item = target.parentElement;
-    item.remove();
-  }
+      tick.addEventListener("click", function() {
+          completed = !completed;
+          li.classList.toggle("completed", completed);
+          tick.classList.toggle("fa-check-square", completed);
+          tick.classList.toggle("fa-square", !completed);
+      });
 
-  if (target.classList.contains("todo-check")) {
-    let item = target.parentElement;
-    item.classList.toggle("completed");
+      deleteIcon.addEventListener("click", function() {
+          li.remove();
+      });
+
+      li.appendChild(tick);
+      li.appendChild(gorevText);
+      li.appendChild(deleteIcon);
+
+      li.addEventListener("animationend", function() {
+          li.classList.remove("animate");
+      });
+
+      gorevListesi.prepend(li);
+      document.getElementById("yeniGorev").value = "";
   }
-});
+}
+
+
+// document.getElementById("yeniGorev").addEventListener("keyup", function(event) {
+//   if (event.key === "Enter") {
+//       gorevEkle();
+//   }
+// });
+
+// 
+
+// 
