5269ed9319910c1ea5ee406dda81ba6816ae628b
mesbol <mesbol6647@gmail.com>
2023-11-07 17:36:53 +0300

    todo

---
+++ TeamWork-todo/project_2/app.js
@@ -0,0 +1,6 @@
+// var kare = document.querySelectorAll(".bosKare");
+// kare.forEach(function(bosKare) {
+//     bosKare.addEventListener("click", function() {
+//         bosKare.classList.toggle("kare");
+//     });
+// });
