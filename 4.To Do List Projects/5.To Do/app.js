4545defb0bae5ba5f62cdd0845315e25bada9c84
Mesut <mesbol6647@gmail.com>
2024-05-18 18:22:54 +0300

    readme added

--- 4.To Do List Projects/5.To Do/app.js
+++
@@ -1,5 +0,0 @@
-function toggleTick(element) {
-    element.querySelector(".tick").classList.toggle("active");
-    element.style.backgroundColor = element.querySelector(".tick").classList.contains("active") ? "#ae5f75" : "#ccc";
-    element.closest(".container").classList.toggle("container-acik");
-}
